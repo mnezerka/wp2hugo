98f4268fdcfa23f0c6f4b0285db68517bd183997
mnezerka <michal.nezerka@sap.com>
2021-11-30 14:46:22 +0100

    fetching images, reorg of export code

--- wordpress/model.go
+++ wordpress/model.go
@@ -18,17 +18,18 @@ type Channel struct {
 }
 
 type Item struct {
-	XMLName    xml.Name       `xml:"item"`
-	Id         int            `xml:"http://wordpress.org/export/1.2/ post_id"`
-	Name       string         `xml:"http://wordpress.org/export/1.2/ post_name"`
-	ParentId   int            `xml:"http://wordpress.org/export/1.2/ post_parent"`
-	Title      string         `xml:"title"`
-	Creator    string         `xml:"http://purl.org/dc/elements/1.1/ creator"`
-	Content    string         `xml:"http://purl.org/rss/1.0/modules/content/ encoded"`
-	Type       string         `xml:"http://wordpress.org/export/1.2/ post_type"`
-	MenuOrder  int            `xml:"http://wordpress.org/export/1.2/ menu_order"`
-	PostDate   wp_date        `xml:"http://wordpress.org/export/1.2/ post_date"`
-	Categories []ItemCategory `xml:"category"`
+	XMLName       xml.Name       `xml:"item"`
+	Id            int            `xml:"http://wordpress.org/export/1.2/ post_id"`
+	Name          string         `xml:"http://wordpress.org/export/1.2/ post_name"`
+	ParentId      int            `xml:"http://wordpress.org/export/1.2/ post_parent"`
+	Title         string         `xml:"title"`
+	Creator       string         `xml:"http://purl.org/dc/elements/1.1/ creator"`
+	Content       string         `xml:"http://purl.org/rss/1.0/modules/content/ encoded"`
+	Type          string         `xml:"http://wordpress.org/export/1.2/ post_type"`
+	MenuOrder     int            `xml:"http://wordpress.org/export/1.2/ menu_order"`
+	PostDate      wp_date        `xml:"http://wordpress.org/export/1.2/ post_date"`
+	Categories    []ItemCategory `xml:"category"`
+	AttachmentUrl string         `xml:"http://wordpress.org/export/1.2/ attachment_url"`
 
 	Attachments []Item
 }
@@ -64,8 +65,16 @@ type ItemCategory struct {
 }
 
 type HugoFrontMatter struct {
-	Title      string   `yaml:"title"`
-	Date       string   `yaml:"date"`
-	Categories []string `yaml:"categories"`
-	Tags       []string `yaml:"tags"`
+	Title      string                    `yaml:"title"`
+	Date       string                    `yaml:"date"`
+	Slug       string                    `yaml:"slug"`
+	Categories []string                  `yaml:"categories"`
+	Tags       []string                  `yaml:"tags"`
+	Resources  []HugoFrontMatterResource `yaml:"resources"`
+}
+
+type HugoFrontMatterResource struct {
+	Src    string                 `yaml:"src"`
+	Title  string                 `yaml:"title"`
+	Params map[string]interface{} `yaml:"params"`
 }
--- wordpress/wpexport.go
+++ wordpress/wpexport.go
@@ -3,9 +3,13 @@ package wordpress
 import (
 	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
 	"github.com/op/go-logging"
@@ -15,6 +19,11 @@ import (
 type WpExport struct {
 	log     *logging.Logger
 	channel *Channel
+
+	hugo_root    string
+	hugo_content string
+	hugo_posts   string
+	hugo_pages   string
 }
 
 func NewWpExport(logger *logging.Logger) *WpExport {
@@ -145,17 +154,7 @@ func (w *WpExport) Export() error {
 		return nil
 	}
 
-	hugo_root := filepath.Join(".", "build")
-	w.ensure_dir(hugo_root)
-
-	hugo_content := filepath.Join(hugo_root, "content")
-	w.ensure_dir(hugo_content)
-
-	hugo_posts := filepath.Join(hugo_content, "posts")
-	w.ensure_dir(hugo_posts)
-
-	hugo_pages := filepath.Join(hugo_content, "pages")
-	w.ensure_dir(hugo_pages)
+	w.prepareDirs()
 
 	ch := w.channel
 	for i := 0; i < len(ch.Items); i++ {
@@ -166,60 +165,172 @@ func (w *WpExport) Export() error {
 			continue
 		}
 
+		// get dir
+		item_dir := w.prepareItemDir(&item)
+
 		// Build Front Matter
 		front_matter := HugoFrontMatter{}
 		front_matter.Title = item.Title
-		front_matter.Date = item.PostDate.Format("2006-02-01")
+		front_matter.Date = item.PostDate.Format("2006-01-02")
+		front_matter.Slug = item.Name
 
-		taxonomies, err := item.GetTaxonomies()
-		w.check(err)
+		w.prepareItemTaxonomies(&item, &front_matter)
 
-		if len(taxonomies["tags"]) > 0 {
-			front_matter.Tags = taxonomies["tags"]
-		}
+		w.prepareItemAttachments(&item, &front_matter, item_dir)
 
-		if len(taxonomies["categories"]) > 0 {
-			front_matter.Categories = taxonomies["categories"]
-		}
-
-		// filename and dir
-		// get year
-		var file_path string
-		switch item.Type {
-		case "post":
-			file_dir_path := filepath.Join(hugo_posts, item.PostDate.Format("2006"))
-			w.ensure_dir(file_dir_path)
-			file_path = filepath.Join(hugo_posts, item.PostDate.Format("2006"), item.Name)
-		case "page":
-			file_path = filepath.Join(hugo_posts)
-		}
-
-		file_path = file_path + ".md"
-
-		w.log.Infof("File to be generated: %s", file_path)
-
-		front_matter_bytes, err := yaml.Marshal(&front_matter)
-		w.check(err)
-
-		f, err := os.Create(file_path)
-		w.check(err)
-
-		// It’s idiomatic to defer a Close immediately after opening a file.
-		defer f.Close()
-
-		w.file_write_str(f, "---\n")
-		_, err = f.Write(front_matter_bytes)
-		w.check(err)
-		w.file_write_str(f, "---\n\n")
-
-		// process item content
-		converter := md.NewConverter("", true, nil)
-
-		content_markdown, err := converter.ConvertString(item.Content)
-		w.check(err)
-
-		w.file_write_str(f, content_markdown)
+		file_path := filepath.Join(item_dir, "index.md")
+		w.writeItem(&item, &front_matter, file_path)
 	}
 
 	return nil
+}
+
+func (w *WpExport) prepareDirs() {
+
+	w.hugo_root = filepath.Join(".", "build")
+	w.ensure_dir(w.hugo_root)
+
+	w.hugo_content = filepath.Join(w.hugo_root, "content")
+	w.ensure_dir(w.hugo_content)
+
+	w.hugo_posts = filepath.Join(w.hugo_content, "posts")
+	w.ensure_dir(w.hugo_posts)
+
+	w.hugo_pages = filepath.Join(w.hugo_content, "pages")
+	w.ensure_dir(w.hugo_pages)
+}
+
+func (w *WpExport) prepareItemDir(item *Item) string {
+
+	// construct file path, starting with proper dir
+	var file_path string
+	switch item.Type {
+	case "post":
+		// create directory derived from post date
+		file_path = filepath.Join(w.hugo_posts, item.PostDate.Format("2006"))
+		file_path = filepath.Join(file_path, item.PostDate.Format("2006_01_02_")+item.Name)
+
+	case "page":
+		file_path = filepath.Join(w.hugo_pages, item.Name)
+	}
+
+	// create single directory for each post/page since we need a bundle (to
+	// be able to store attachments)
+	w.ensure_dir(file_path)
+
+	return file_path
+}
+
+func (w *WpExport) prepareItemAttachments(item *Item, fh *HugoFrontMatter, item_dir string) {
+	attachments := w.FindAttachments(item.Id)
+
+	for i := 0; i < len(attachments); i++ {
+		a := attachments[i]
+		file_name := path.Base(a.AttachmentUrl)
+		file_ext := filepath.Ext(file_name)
+		target_file_name := strings.ToLower(file_name)
+
+		w.log.Infof("Processing attachment %s", file_name)
+
+		switch file_ext {
+		case ".jpg", ".jpeg", ".png":
+
+			r := HugoFrontMatterResource{
+				Src:    filepath.Join("images", target_file_name),
+				Title:  a.Content,
+				Params: make(map[string]interface{}),
+			}
+
+			target_dir := filepath.Join(item_dir, "images")
+			w.ensure_dir(target_dir)
+
+			target_file_path := filepath.Join(target_dir, target_file_name)
+
+			// fetch file and store it
+			w.downloadFile(a.AttachmentUrl, target_file_path)
+
+			r.Params["weight"] = a.MenuOrder
+			fh.Resources = append(fh.Resources, r)
+
+		case ".gpx":
+
+			target_dir := filepath.Join(item_dir, "gpx")
+			w.ensure_dir(target_dir)
+
+			target_file_path := filepath.Join(target_dir, target_file_name)
+
+			// fetch file and store it
+			w.downloadFile(a.AttachmentUrl, target_file_path)
+		default:
+			w.log.Warningf("Unknow attachment type %s (%s)", file_name, a.AttachmentUrl)
+		}
+
+	}
+}
+
+func (w *WpExport) prepareItemTaxonomies(item *Item, fm *HugoFrontMatter) {
+
+	taxonomies, err := item.GetTaxonomies()
+	w.check(err)
+
+	if len(taxonomies["tags"]) > 0 {
+		fm.Tags = taxonomies["tags"]
+	}
+
+	if len(taxonomies["categories"]) > 0 {
+		fm.Categories = taxonomies["categories"]
+	}
+
+}
+
+func (w *WpExport) writeItem(item *Item, fm *HugoFrontMatter, file_path string) {
+
+	w.log.Infof("Writing item data to file: %s", file_path)
+
+	front_matter_bytes, err := yaml.Marshal(fm)
+	w.check(err)
+
+	f, err := os.Create(file_path)
+	w.check(err)
+
+	// It’s idiomatic to defer a Close immediately after opening a file.
+	defer f.Close()
+
+	w.file_write_str(f, "---\n")
+	_, err = f.Write(front_matter_bytes)
+	w.check(err)
+	w.file_write_str(f, "---\n\n")
+
+	// process item content
+	converter := md.NewConverter("", true, nil)
+
+	content_markdown, err := converter.ConvertString(item.Content)
+	w.check(err)
+
+	w.file_write_str(f, content_markdown)
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func (w *WpExport) downloadFile(url string, file_path string) {
+
+	// check if local file exists
+	if _, err := os.Stat(file_path); err == nil {
+		w.log.Warningf("File %s exists, keeping existing content (no overwrite)", file_path)
+		return
+	}
+
+	resp, err := http.Get(url)
+	w.check(err)
+
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(file_path)
+	w.check(err)
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	w.check(err)
 }
